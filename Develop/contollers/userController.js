27195e20d7c04de48e887024349b3cd74b05cf2e
eddiespag-hetti <eddie_p89@hotmail.com>
2024-04-07 15:38:17 +1000

    added functions to user controller

--- Develop/contollers/userController.js
+++ Develop/contollers/userController.js
@@ -1,2 +1,50 @@
 const { ObjectId } = require('mongoose').Types;
 const { User, Thought } = require('../models');
+
+
+
+module.exports = {
+// Get all Users 
+ async getUsers (req, res) {
+    try {
+        const users = await User.find();
+        res.status(200).json(users)
+
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err)
+    } 
+},
+
+// Get a single User
+async getSingleUser(req, res) {
+    try {
+      const user = await User.findOne({ _id: req.params.userId })
+        .select('-__v');
+
+      if (!user) {
+        return res.status(404).json({ message: 'No user with that ID' })
+      }
+    }
+    catch (err) {
+      res.status(500).json({ err });
+      return;
+    }
+    
+    res.status(200).json(user)
+},
+
+// Create a new User
+ 
+ async createUser(req, res) {
+    try {
+      const user = await User.create(req.body);
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+
+}
--- Develop/routes/api/userRoutes.js
+++ Develop/routes/api/userRoutes.js
@@ -2,18 +2,6 @@ const router = require('express').Router();
 
 const { User } = require('../../models')
 
-router.get('/', async (req, res) => {
-    try {
-        const users = await User.find();
-        res.status(200).json(users)
-
-
-    } catch (err) {
-        console.log(err);
-        res.status(500).json(err)
-    } 
-})
-
 
 
 
